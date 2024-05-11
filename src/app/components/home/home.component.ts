1cf7e30cf7eb430be337c25d4940ac6d40c2d22e
Raphael <ex121529@edp.pt>
2024-05-11 06:26:04 +0100

    dashboard card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { HomeFiltersComponent } from './components/home-filters/home-filters.component';
+import { HomePetCardComponent } from './components/home-pet-card/home-pet-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    HomeBannerComponent
+    HomeBannerComponent,
+    HomeFiltersComponent,
+    HomePetCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
