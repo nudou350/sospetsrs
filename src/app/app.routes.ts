9c662bf270af15d4aff7de88e3fe5b98dc873314
Raphael <ex121529@edp.pt>
2024-05-13 00:59:38 +0100

    shelter base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
     loadComponent: () => import('./components/password-recovery/password-recovery.component').then(m => m.PasswordRecoveryComponent)
   },
   {
-    path: 'abrigo',
+    path: 'abrigos',
     loadComponent: () => import('./components/abrigo/abrigo.component').then(m => m.AbrigoComponent)
   },
   {
