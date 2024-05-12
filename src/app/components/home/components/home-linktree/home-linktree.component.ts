2f47a7b98fcb2f8d39a6aea7eab7bd2caa74099c
Raphael <ex121529@edp.pt>
2024-05-12 06:22:46 +0100

    update text carine

--- src/app/components/home/components/home-linktree/home-linktree.component.ts
+++ src/app/components/home/components/home-linktree/home-linktree.component.ts
@@ -16,32 +16,32 @@ import { RouterLink } from '@angular/router';
 export class HomeLinktreeComponent {
   actions = [
     {
-      text: "Ver abrigos cadastrados",
+      text: "Situação atual dos abrigos",
       icon: 'shelter',
       url: '/abrigos'
     },
     {
-      text: "Ver mural de animais encontrados",
-      icon: 'search',
+      text: "Cadastre um animal perdido",
+      icon: 'lost-pet',
       url: 'https://www.petsos.com.br/'
     },
     {
-      text: "Cadastrar animal perdido",
-      icon: 'lost-pet',
-      url: 'https://www.petsos.com.br/cadastro'
-    },
-    {
-      text: "Cadastrar animal encontrado",
+      text: "Cadastre um animal encontrado",
       icon: 'found',
       url: 'https://www.petsos.com.br/cadastro'
     },
     {
-      text: "Solicitar resgate",
+      text: "Peça um resgate",
       icon: 'sos',
-      url: '/'
+      url: 'https://www.petsos.com.br/cadastro'
     },
     {
-      text: "Quero ser um lar temporário",
+      text: "Veja os animais encontrados",
+      icon: 'search',
+      url: 'https://www.petsos.com.br/'
+    },
+    {
+      text: "Quero adotar",
       icon: 'temporary-home',
       url: '/'
     },
