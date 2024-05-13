b84f1d865b590951a1bfe949c19caa121d9862fd
Raphael <ex121529@edp.pt>
2024-05-13 08:49:47 +0100

    loading service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,12 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors, withFetch, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpRequestInterceptor } from './core/interceptors/httpRequestInterceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient(withFetch(), withInterceptors([HttpRequestInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([HttpRequestInterceptor, loadingInterceptor])),
   ]
 };
