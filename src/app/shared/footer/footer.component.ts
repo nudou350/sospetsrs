be8f3002a05567163851223e3f00fdc520863dfc
Fabio Fabian <53060721+fabiofabian@users.noreply.github.com>
2024-05-13 03:45:32 -0300

    Placeholder adjusts and dynamic year on footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
@@ -14,9 +16,10 @@ import { AuthService } from '../../services/auth.service';
 })
 export class FooterComponent { 
   #authService = inject(AuthService)
+  currentYear: number = new Date().getFullYear()
 
   loggedIn = this.#authService.loggedIn
-  
+
   logout(){
     this.#authService.logout()
   }
