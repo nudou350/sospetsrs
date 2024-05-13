987b0fbb6fd715c371014cb7cd37a072f9162735
Fabio Fabian <53060721+fabiofabian@users.noreply.github.com>
2024-05-13 01:28:28 -0300

    httpInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequestInterceptor } from './core/interceptors/httpRequestInterceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideHttpClient(withFetch(), withInterceptors([HttpRequestInterceptor])),
+  ]
 };
