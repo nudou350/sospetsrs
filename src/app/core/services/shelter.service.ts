1fa1c6803c1a472900bf474bc0c757320d68a1cb
Raphael <ex121529@edp.pt>
2024-05-13 10:43:31 +0100

    searchbar

--- src/app/core/services/shelter.service.ts
+++ src/app/core/services/shelter.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { IShelterInterface } from '../../components/abrigo/dto/shelter.dto';
 import { HttpService } from '../http/http.service';
@@ -10,23 +10,27 @@ import { HttpService } from '../http/http.service';
 })
 export class ShelterService {
   #http = inject(HttpService)
+  //create a signal to store the shelters so we dont have to keep calling API later
+  #shelters = signal<IShelterInterface[]>([]);
+  shelters = this.#shelters.asReadonly()
 
-  getShelters(): Observable<IShelterInterface[]>{
-    return this.#http.get<{data: IShelterInterface[]}>(`${environment.apiUrl}/shelters`).pipe(
-      map(res => res.data));
+  getShelters(): Observable<IShelterInterface[]> {
+    return this.#http.get<{ data: IShelterInterface[] }>(`${environment.apiUrl}/shelters`).pipe(
+      map(res => res.data),
+      tap(shelters => this.#shelters.set(shelters)))
   }
 
-  getShelterById(id: number): Observable<IShelterInterface>{
+  getShelterById(id: number): Observable<IShelterInterface> {
     return this.#http.get<IShelterInterface>(`${environment.apiUrl}/shelters/${id}`).pipe(
       map(res => res));
   }
 
-  updateShelter(id: number, shelter: IShelterInterface): Observable<IShelterInterface>{
+  updateShelter(id: number, shelter: IShelterInterface): Observable<IShelterInterface> {
     return this.#http.put<IShelterInterface>(`${environment.apiUrl}/shelters/${id}`, shelter).pipe(
       map(res => res));
   }
-  
-  createShelter(shelter: Partial<IShelterInterface>): Observable<IShelterInterface>{
+
+  createShelter(shelter: Partial<IShelterInterface>): Observable<IShelterInterface> {
     return this.#http.post<IShelterInterface>(`${environment.apiUrl}/shelters`, shelter).pipe(
       map(res => res));
   }
