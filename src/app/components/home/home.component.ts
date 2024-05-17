0e31fb5ce9fd43fb11d864dc955d30839cfad62c
tayna alves <tayna.tech@gmail.com>
2024-05-17 16:51:23 -0300

    feature: add in developmento home section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
 import { HomeLinktreeComponent } from './components/home-linktree/home-linktree.component';
+import { HomeInDevelopmentComponent } from './components/home-in-development/home-in-development.component';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,8 @@ import { HomeLinktreeComponent } from './components/home-linktree/home-linktree.
   imports: [
     CommonModule,
     HomeBannerComponent,
-    HomeLinktreeComponent
-
+    HomeLinktreeComponent,
+    HomeInDevelopmentComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
