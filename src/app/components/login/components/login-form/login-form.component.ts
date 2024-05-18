48620c51ddb18788af400ea460e51b3004015c17
Gabriel Silva de Jesus <gabriel.jesus@sergipetec.org.br>
2024-05-17 22:56:14 -0300

    feat: show or hidde password

--- src/app/components/login/components/login-form/login-form.component.ts
+++ src/app/components/login/components/login-form/login-form.component.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component, TemplateRef, ViewChild, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  TemplateRef,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
 import { ToastService } from '../../../../core/services/toast.service';
 import { clippingParents } from '@popperjs/core';
-
 
 @Component({
   selector: 'app-login-form',
@@ -13,20 +23,20 @@ import { clippingParents } from '@popperjs/core';
   styleUrls: ['./login-form.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ReactiveFormsModule, CommonModule, RouterModule]
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
 })
 export class LoginFormComponent {
   loginForm: FormGroup;
-  #toastService = inject(ToastService)
+  #toastService = inject(ToastService);
+  hidePassword: boolean = true;
 
   @ViewChild('successTpl') successTpl!: TemplateRef<any>;
   @ViewChild('errorTpl') errorTpl!: TemplateRef<any>;
 
-
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required]],
     });
   }
 
@@ -34,31 +44,46 @@ export class LoginFormComponent {
     if (this.loginForm.valid) {
       this.authService.login(this.loginForm.value).subscribe({
         next: (response) => {
-          this.#toastService.showSuccess('Login bem sucedido!'); 
+          this.#toastService.showSuccess('Login bem sucedido!');
           console.log('Login successful', response);
         },
         error: (error) => {
-          this.#toastService.showError('Erro ao logar! Verifique seu email ou senha');
+          this.#toastService.showError(
+            'Erro ao logar! Verifique seu email ou senha'
+          );
           console.error('Login failed', error);
-        }
+        },
       });
     }
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  get showWarnningRulePass() {
+    return (
+      this.loginForm.get('password')?.errors?.['required'] &&
+      this.loginForm.get('password')?.touched
+    );
   }
 
   onRequestResetPassword() {
     //TODO: Melhorar fluxo, adicionar input de email na tela de recuperação talvez
     if (!this.loginForm.get('email')?.valid) {
-      alert("Necessário preencher o e-mail para recuperar a senha!");
+      alert('Necessário preencher o e-mail para recuperar a senha!');
       return;
     }
 
-    this.authService.requestResetPassword({ email: this.loginForm.value['email'] }).subscribe({
-      next: (response) => {
-        console.log('Request reset password successful', response);
-      },
-      error: (error) => {
-        console.error('Request failed', error);
-      }
-    });
+    this.authService
+      .requestResetPassword({ email: this.loginForm.value['email'] })
+      .subscribe({
+        next: (response) => {
+          console.log('Request reset password successful', response);
+        },
+        error: (error) => {
+          console.error('Request failed', error);
+        },
+      });
   }
 }
